f562aa8dd794dba76185a30a198be7b984d68d8d
ARK2306 <aryanreddykalluri@gmail.com>
2026-05-21 17:06:02 -0500

    made edits

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://ark2306.github.io",
+  base: "/portfolio-dev",
   vite: {
     plugins: [tailwindcss()],
   },
